bcd87f6afc0e8faff80f535ede71100a3610d230
ali ustunyer <you@example.com>
2023-04-14 23:01:06 -0500

    second commit for week 14

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -2,10 +2,15 @@ package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.math.BigDecimal;
+import java.util.LinkedList;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
@@ -26,12 +31,39 @@ class FetchJeepTest extends FetchJeepTestSupport{
     //System.out.println(uri);
     
     //When :  a connection is made to the URI
-    ResponseEntity<Jeep> response = 
-    getRestTemplate().getForEntity(uri, Jeep.class);
+    ResponseEntity<List<Jeep>> response = 
+    getRestTemplate().exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
     
     //Then : a success (OK-200) status code is returned
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     
+    //And : the actual list returned is the same as the expected list
+    List<Jeep> expected = buildExpected();
+    assertThat(response.getBody()).isEqualTo(expected);
+  }
+
+  private List<Jeep> buildExpected() {
+    List<Jeep> list = new LinkedList<>();
+    
+    //@formatter : off
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(2)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("28475.00"))
+        .build());
+        
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(4)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("31975.00"))
+        .build());
+  //@formatter : off
+    
+    return list;
   }
 
 }
